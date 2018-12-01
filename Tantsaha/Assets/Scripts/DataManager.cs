e450bbd09dee30976b9c1f7d9624ddc6d96b3819
menkaix <mendrika.irina@gmail.com>
2018-12-01 15:00:00 +0100

    piou

---
+++ Tantsaha/Assets/Scripts/DataManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour {
+
+	public string url;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(getData());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	IEnumerator getData()
+	{
+		//tantsaha.alphagroup.mg/lib/vendor/controls/getter.php?zone=x&surface=y&semence=x
+		WWWForm form = new WWWForm();
+		form.AddField("zone","");
+		form.AddField("surface", "");
+		form.AddField("semence", "");
+
+		WWW myWWW = new WWW(url, form);
+
+		yield return myWWW;
+
+		if(myWWW.error != null && myWWW.error != "")
+		{
+			Debug.LogError(myWWW.error);
+		}
+		else
+		{
+			
+			Debug.Log(myWWW.text);
+		}
+
+
+	}
+}
---
+++ Tantsaha/Assets/Scripts/RegionSelector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegionSelector : MonoBehaviour {
+
+	private Text display;
+
+	public int currentIndex = 0;
+	public string[] regions;
+
+	public void incIndex()
+	{
+		currentIndex++;
+		if(currentIndex>= regions.Length)
+		{
+			currentIndex = 0;
+		}
+	}
+
+	public void decIndex()
+	{
+		currentIndex--;
+		if (currentIndex < 0)
+		{
+			currentIndex = regions.Length - 1;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		display = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		display.text = regions[currentIndex];
+	}
+}
